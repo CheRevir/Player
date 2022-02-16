0dfa1db196d74dcad48aa595d0d9c2b4620f961b
CheRevir <CheRevir@gmail.com>
2022-02-16 15:44:37 +0800

    Add App

---
+++ app/src/main/java/com/cere/player/App.kt
@@ -0,0 +1,12 @@
+package com.cere.player
+
+import android.app.Application
+
+/**
+ * Created by CheRevir on 2022-02-16
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
